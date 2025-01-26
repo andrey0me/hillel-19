612605ae033496940995ea4b3643c1fc374e9e0e
andrey0me <andrey.dnepr.1995@gmail.com>
2025-01-26 15:51:55 +0200

    Add folder lesson-Final

---
+++ app/js/script.js
@@ -0,0 +1,10 @@
+$('.js-slider').slick({
+  dots: false,
+  infinite: false,
+  arrows: false,
+  speed: 200,
+  slidesToShow: 2,
+  centerMode: false,
+  variableWidth: true,
+  mobileFirst: true
+});
